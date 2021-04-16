390ffacc60b49e31809a9c566aa066a6546767d7
M. Ian Graham <games@miangraham.com>
2021-04-16 21:33:05 +0900

    oxalica overlay working

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,16 +1,12 @@
 let
   sources = import ./sources.nix;
-  moz-overlay = import sources.nixpkgs-mozilla;
+  oxa-overlay = import sources.oxalica-overlay;
   rust-overlay = self: super:
-    let channel = (super.rustChannelOf {
-          channel = "1.48.0";
-          # channel = "1.51.0";
-          # sha256 = "14qhjdqr2b4z7aasrcn6kxzj3l7fygx4mpa5d4s5d56l62sllhgq";
-        });
+    let channel = super.rust-bin.stable."1.51.0".default;
     in
       {
-        rustc = channel.rust;
-        inherit (channel) cargo;
+        cargo = channel;
+        rustc = channel;
       };
 in
-[ moz-overlay rust-overlay ]
+[ oxa-overlay rust-overlay ]
